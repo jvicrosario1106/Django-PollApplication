a4993ff8f61a55aefa00a8670e1a760274c2142f
jvicrosario1106 <jvrosario1106@gmail.com>
2020-08-18 15:28:39 +0800

    Settings.py

--- pollproject/settings.py
+++ pollproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("POLL_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["djangopoll.herokuapp.com", "*"]
+ALLOWED_HOSTS = ["djangopoll.herokuapp.com"]
 
 
 # Application definition
